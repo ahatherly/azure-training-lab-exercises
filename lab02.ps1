2486ec6fe81fe7200cf1ef7dcdb1bb0dc9e6ebd9
Adam Hatherly <adam.hatherly@nhs.net>
2018-02-26 16:29:17 +0000

    Initial commit

---
+++ lab02.ps1
@@ -0,0 +1,11 @@
+#az login
+
+#az group create --name MOD02APPS --location eastus
+
+#az group deployment create --name ExampleDeployment --resource-group MOD02STOR --template-file lab02-template.json
+
+#az group list
+
+#az group delete --name MOD02STOR --no-wait --yes
+
+#az group delete --name MOD02APPS --no-wait --yes
